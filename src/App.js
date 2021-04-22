34513408166b72c2f8164118a4a00a0b6d8199a2
Beth Franklin <66703062+redliz5808@users.noreply.github.com>
2021-04-22 17:13:04 -0400

    Created home page and started individual photo page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import User from "./pages/User";
+import Photo from "./pages/Photo";
+import logo from "./assets/AThousandWords.png";
+import { Logo } from "./app.styles";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">
+            <Logo src={logo} alt="logo" />
+          </Link>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/user/:userid">User</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route component={Home} exact path="/"></Route>
+          <Route component={User} path="/user/:userid"></Route>
+          <Route component={Photo} path="photo/:photoid"></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/pages/Home/home.styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const StyledLink = styled(Link)`
+  display: inline;
+  text-decoration: none;
+`
